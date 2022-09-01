f3c4f2d01a68a573eb78e255c14b5e7e36d1ec03
RoyReyesisdr <A01705292@tec.mx>
2022-09-01 16:35:05 -0500

    Peticiones http

---
+++ app.js
@@ -0,0 +1,25 @@
+const filesystem = require('fs')
+filesystem.writeFileSync('hola.txt','Hola desde node')
+
+
+console.log("hola desde node")
+const arreglo = [5000, 60, 50, 100, 10 ,20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for(let item in arreglo){
+    console.log(item);
+}
+for(let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+    
+}
+
+const http = require('http');
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+ // response.setHeader("Content-Type, text/html");
+ // response.write()
+ // response.end();   
+});
+server.listen(3000);
